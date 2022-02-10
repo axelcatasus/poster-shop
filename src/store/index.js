1c552a590159af8b652779fa5187e449a8ae6eab
axelcatasus <84475869+axelcatasus@users.noreply.github.com>
2022-02-10 11:29:12 +0100

    Issue #10 (Added v-for to Home Component)

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import products from '@/assets/products.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: [...products]
   },
   mutations: {
   },
