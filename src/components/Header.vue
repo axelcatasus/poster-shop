bc0bb90230bed33c8c8886f4d5b9a17ffe771755
axelcatasus <84475869+axelcatasus@users.noreply.github.com>
2022-02-10 11:16:35 +0100

    Additional styling and add h1 on Header component
    Issue #6

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="header">
+    <h1 class="header--heading">Poster Shop</h1>
           
   </div>
 </template>
@@ -13,9 +14,17 @@ export default {
 <style lang='scss' scpoed>
 
 .header {
-  height: 608px;
+  min-height: 608px;
+  display: grid;
+  justify-content: center;
+  align-content: center;
   width: 100%;
   background: #3DE07E url('../assets/ninjahead.svg') center bottom no-repeat
+}
+
+.header--heading {
+  font-size: 102px;
+  color: #222222
 }
 
 </style>
