79797f3d144576976be9182280c7198d7d4a1b41
OscarArr <oscar.arrhenius@gmail.com>
2022-02-10 14:46:46 +0100

    product list -added info to product-cards and styled product-card -added list to print out product cards in home -styled main page solved #9, solved #10, solved #8, solved #13

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="home">
-    <article v-for='product in products' :key="product.id">
-      <p>{{product.title}}</p>
-    </article>
+    <div class="products">
+      <Product 
+        :character="product"
+        v-for='product in products'
+        :key="product.id"
+      />
+    </div>
+      <div class="large-products">
+        <Product
+        :character="product"
+        v-for='product in bigProducts'
+        :key='product.id'
+        :big="true"
+        />
+      </div>
   </div>
 </template>
 
 <script>
+import Product from '../components/Product.vue'
 
 export default {
+  components: { Product },
   computed: {
     products(){
-      return this.$store.state.products
+      let productsArray = []
+      for(let i = 0; i < this.$store.state.products.length-2; i++){
+        productsArray.push(this.$store.state.products[i])
+      }
+      return productsArray
+    },
+    bigProducts(){
+       let bigProductsArray = []
+      for(let i = this.$store.state.products.length-2; i < this.$store.state.products.length; i++){
+        bigProductsArray.push(this.$store.state.products[i])
+      }
+      return bigProductsArray
     }
   }
 }
@@ -21,9 +46,24 @@ export default {
 <style lang="scss" scoped>
 
 .home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background: #222222;
-  height: 400px;
   width: 100%;
+  padding-top: 96px;
+  padding-bottom: 96px
 }
 
+.products {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 32px;
+}
+
+.large-products {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 32px;
+}
 </style>
