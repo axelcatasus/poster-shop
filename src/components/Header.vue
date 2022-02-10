fa6232276ebecc118c8147574c91149bbace8073
StellaSFE96 <stella.samaranayake@iths.se>
2022-02-10 11:42:41 +0100

    complete sidebar component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="header">
+<<<<<<< HEAD
     <router-link to="/MenuBar"><img class="burger" src="../assets/navicon.svg" alt="burger menu"></router-link>
+=======
+    <h1 class="header--heading">Poster Shop</h1>
+          
+>>>>>>> 93f8e5be013438f53332afb20eaf53f2153604d1
   </div>
 </template>
 
@@ -13,14 +18,24 @@ export default {
 <style lang='scss' scpoed>
 
 .header {
-  height: 608px;
+  min-height: 608px;
+  display: grid;
+  justify-content: center;
+  align-content: center;
   width: 100%;
   background: #3DE07E url('../assets/ninjahead.svg') center bottom no-repeat
 }
 
+<<<<<<< HEAD
 .burger{
     display: flex;
     padding: 30px;
   }
+=======
+.header--heading {
+  font-size: 102px;
+  color: #222222
+}
+>>>>>>> 93f8e5be013438f53332afb20eaf53f2153604d1
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <article v-for='product in products' :key="product.id">
+      <p>{{product.title}}</p>
+    </article>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  computed: {
+    products(){
+      return this.$store.state.products
+    }
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+
+.home {
+  background: #222222;
+  height: 400px;
+  width: 100%;
+}
+
+</style>
